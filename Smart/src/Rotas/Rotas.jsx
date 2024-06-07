577128d42deb2ec569c42de65b7c0c3a501fde3c
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-06-07 11:09:59 -0300

    integradorFront

---
+++ Smart/src/Rotas/Rotas.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import { Login } from '../Paginas/Login';
+import { Sensor } from '../Paginas/Sensor';
+import { Inicial } from '../Paginas/Inicial';
+import { CadastrarSensor } from '../Paginas/CadastrarSensor';
+import { Localizacao } from '../Paginas/Localizacao';
+import { Filtro } from '../Paginas/Filtro'; 
+import { AlterarSensor } from '../Paginas/AlterarSensor';
+
+ export function Rotas() {
+    return (
+        <Routes>
+
+        <Route path='/' element={ <Login /> } />        
+
+        <Route path='inicial' element={ <Inicial /> } >
+            <Route index element={ <Sensor /> } />
+            <Route path='cadsensor' element={ <CadastrarSensor /> } />
+            <Route path='localizacao' element={ <Localizacao /> } />
+            <Route path='filtro' element={<Filtro />} />        
+            <Route path="alterar-sensor/:id" element={<AlterarSensor />} />
+            
+        </Route>
+
+    </Routes>
+)
+}
+
+
